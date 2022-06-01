84d87199fa28135f44c87dc693b2d1d191203e7e
jurge <jurge@DESKTOP-SEIVM4T>
2022-06-01 18:52:11 -0500

    Avance en el diseño de la ventana - Modifiqué la clase Auto, agregue atributos, sus métodos Set y Get y otros métodos para la clase. - Modifiqué la clase Ventana agregando las etiquetas, campos de texto y botones con su diseño correspondiente.

--- src/programa/Auto.java
+++ src/programa/Auto.java
@@ -4,5 +4,52 @@ public class Auto {
     
     private String placas, nombre;
     private int precio;
+    private int TARIFA = 12;    //Tarifa aplicada por cada hora transcurrida
+    private int tiempo;
+
+    public Auto(String placas, String nombre) {
+        this.placas = placas;
+        this.nombre = nombre;
+        this.precio = 0;
+        this.tiempo = 0;
+    }
+
+    //Getters y Setters
+    public String getPlacas() {
+        return placas;
+    }
+
+    public void setPlacas(String placas) {
+        this.placas = placas;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(int precio) {
+        this.precio = precio;
+    }
+
+    public int getTiempo() {
+        return tiempo;
+    }
+
+    public void setTiempo(int tiempo) {
+        this.tiempo = tiempo;
+    }
     
+    //Otros Métodos
+    public void calcularPrecio(){
+        //Se obtiene el precio dado el tiempo ocupado por el auto (Tiempo en horas)
+        this.precio = TARIFA * this.tiempo;
+    }
 }
--- src/programa/Ventana.java
+++ src/programa/Ventana.java
@@ -1,15 +1,22 @@
 package programa;
 
+import java.awt.Color;
+import java.awt.Font;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.JSeparator;
 import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
 import javax.swing.table.DefaultTableModel;
 
 public class Ventana extends JFrame {
@@ -42,22 +49,76 @@ public class Ventana extends JFrame {
     }
 
     private void iniciarBotones() {
-
+        JButton bInsertar = new JButton();
+        bInsertar.setText("Insertar");
+        bInsertar.setBounds(275, 10, 190, 30);
+        bInsertar.setBackground(Color.decode("#24A745"));
+        bInsertar.setForeground(Color.white);
+        bInsertar.setFont(new Font("Roboto", 0, 15));
+        panel.add(bInsertar);
+        
+        JButton bBuscar = new JButton();
+        bBuscar.setText("Buscar");
+        bBuscar.setBounds(275, 50, 190, 30);
+        bBuscar.setBackground(Color.decode("#FCC00D"));
+        bBuscar.setFont(new Font("Roboto", 0, 15));
+        panel.add(bBuscar);
+        
+        JButton bEliminar = new JButton();
+        bEliminar.setText("Eliminar");
+        bEliminar.setBounds(275, 90, 190, 30);
+        bEliminar.setBackground(Color.decode("#DC3445"));
+        bInsertar.setFont(new Font("Roboto", 0, 15));
+        bEliminar.setForeground(Color.white);
+        panel.add(bEliminar);
+        
+        
+        
     }
 
     private void iniciarEtiquetas() {
-
+        //Etiqueta de Placas
+        JLabel ePlacas = new JLabel();
+        ePlacas.setText("Placas: ");
+        ePlacas.setBounds(5, 40, 100, 20);
+        ePlacas.setHorizontalAlignment(SwingConstants.CENTER);
+        ePlacas.setForeground(Color.black);
+        ePlacas.setFont(new Font("Roboto", 0, 15));
+        panel.add(ePlacas);
+        
+        //Etiqueta de Nombre
+        JLabel eNombre = new JLabel();
+        eNombre.setText("Nombre: ");
+        eNombre.setBounds(5, 80, 100, 20);
+        eNombre.setHorizontalAlignment(SwingConstants.CENTER);
+        eNombre.setForeground(Color.black);
+        eNombre.setFont(new Font("Roboto", 0, 15));
+        panel.add(eNombre);
     }
 
     private void iniciarCuadrosTexto() {
-
+        //Campo de texto para placas
+        JTextField campoPlacas = new JTextField();
+        campoPlacas.setBounds(105, 35, 150, 30);
+        panel.add(campoPlacas);
+        
+        //Campo de texto para nombre
+        JTextField campoNombre = new JTextField();
+        campoNombre.setBounds(105, 75, 150, 30);
+        panel.add(campoNombre);
+        
     }
 
     private void iniciarTabla() {
         modeloT = new DefaultTableModel();
         tabla = new JTable(modeloT);
         JScrollPane scroll = new JScrollPane(tabla);
-        scroll.setBounds(5, 80, 250, 470);
+        scroll.setBounds(5, 140, 250, 410);
+        
+        //Columnas de la tabla
+        String[] columnas = new String[]{"Clave", "Valores"};
+        modeloT.setColumnIdentifiers(columnas);
+        tabla.setModel(modeloT);
 
         panel.add(scroll);
     }
