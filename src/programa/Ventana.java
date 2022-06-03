d42b992b6d377fd028e66a0f3ddba7b97a7efe03
NOCT <NOCT@NOCT-0T5NR5K.DL>
2022-06-02 19:27:05 -0500

    Implementacion de archivos para el almacenamiento de los datos

--- src/programa/Ventana.java
+++ src/programa/Ventana.java
@@ -1,13 +1,14 @@
 package programa;
 
+import datos.Hash;
 import java.awt.Color;
 import java.awt.Font;
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -24,6 +25,10 @@ public class Ventana extends JFrame {
     private JPanel panel;
     private JTable tabla;
     private DefaultTableModel modeloT;
+    private Hash[] tablaHash;
+    private File archivo;
+
+    public static final int TAM = 101;
 
     public Ventana() {
         super("Proyecto final");
@@ -31,6 +36,9 @@ public class Ventana extends JFrame {
         this.setLocationRelativeTo(null);
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setResizable(false);
+
+        archivo = new File("Auto.txt");
+        tablaHash = new Hash[TAM];
 
         panel = new JPanel();
         panel.setLayout(null);
@@ -45,7 +53,7 @@ public class Ventana extends JFrame {
         iniciarEtiquetas();
         iniciarCuadrosTexto();
         iniciarTabla();
-//        leer();
+//        leer();   //Reservado para cuando se implementen las acciones de los botones
     }
 
     private void iniciarBotones() {
@@ -56,14 +64,14 @@ public class Ventana extends JFrame {
         bInsertar.setForeground(Color.white);
         bInsertar.setFont(new Font("Roboto", 0, 15));
         panel.add(bInsertar);
-        
+
         JButton bBuscar = new JButton();
         bBuscar.setText("Buscar");
         bBuscar.setBounds(275, 50, 190, 30);
         bBuscar.setBackground(Color.decode("#FCC00D"));
         bBuscar.setFont(new Font("Roboto", 0, 15));
         panel.add(bBuscar);
-        
+
         JButton bEliminar = new JButton();
         bEliminar.setText("Eliminar");
         bEliminar.setBounds(275, 90, 190, 30);
@@ -71,9 +79,7 @@ public class Ventana extends JFrame {
         bInsertar.setFont(new Font("Roboto", 0, 15));
         bEliminar.setForeground(Color.white);
         panel.add(bEliminar);
-        
-        
-        
+
     }
 
     private void iniciarEtiquetas() {
@@ -85,7 +91,7 @@ public class Ventana extends JFrame {
         ePlacas.setForeground(Color.black);
         ePlacas.setFont(new Font("Roboto", 0, 15));
         panel.add(ePlacas);
-        
+
         //Etiqueta de Nombre
         JLabel eNombre = new JLabel();
         eNombre.setText("Nombre: ");
@@ -101,12 +107,12 @@ public class Ventana extends JFrame {
         JTextField campoPlacas = new JTextField();
         campoPlacas.setBounds(105, 35, 150, 30);
         panel.add(campoPlacas);
-        
+
         //Campo de texto para nombre
         JTextField campoNombre = new JTextField();
         campoNombre.setBounds(105, 75, 150, 30);
         panel.add(campoNombre);
-        
+
     }
 
     private void iniciarTabla() {
@@ -114,7 +120,7 @@ public class Ventana extends JFrame {
         tabla = new JTable(modeloT);
         JScrollPane scroll = new JScrollPane(tabla);
         scroll.setBounds(5, 140, 250, 410);
-        
+
         //Columnas de la tabla
         String[] columnas = new String[]{"Clave", "Valores"};
         modeloT.setColumnIdentifiers(columnas);
@@ -136,21 +142,57 @@ public class Ventana extends JFrame {
     }
 
     private void leer() {
-        File archivo = new File("Auto.txt");
-        try {
-            if (!archivo.exists()) {
-                archivo.createNewFile();
-                PrintWriter escribir = new PrintWriter(new FileWriter(archivo));
-                for (int i = 0; i < 100; i++) {
-                    escribir.println(i);
+        if (archivo.exists()) {
+            FileInputStream archivoLectura;
+            ObjectInputStream leer;
+            try {
+                archivoLectura = new FileInputStream(archivo);
+                leer = new ObjectInputStream(archivoLectura);
+                Hash celda;
+                int i = 0;
+                while (i < tablaHash.length) {
+                    celda = (Hash) leer.readObject();
+                    tablaHash[i] = celda;
+                    i++;
                 }
-                escribir.close();
+                for (Hash hash : tablaHash) {
+                    System.out.println(hash);
+                }
+
+            } catch (IOException | ClassNotFoundException ex) {
+                System.err.println(ex);
             }
-            
-            BufferedReader leer = new BufferedReader(new FileReader(archivo));
+        } else {
+            try {
+                archivo.createNewFile();
+                for (int i = 0; i < tablaHash.length; i++) {
+                    tablaHash[i] = new Hash();
+                }
+                escribir();
+            } catch (IOException ex) {
+                System.err.println(ex);
+            }
+        }
+    }
+
+    private int escribir() {
+        int estado = 1;
+        FileOutputStream archivoEscritura;
+        ObjectOutputStream escribir;
+
+        try {
+            archivoEscritura = new FileOutputStream(archivo, false);
+            escribir = new ObjectOutputStream(archivoEscritura);
+            for (Hash hash : tablaHash) {
+                escribir.writeObject(hash);
+            }
+            escribir.close();
+            archivoEscritura.close();
         } catch (IOException ex) {
-            System.err.println("Error");
+            estado = -1;
+            System.err.println(ex);
         }
 
+        return estado;
     }
 }
